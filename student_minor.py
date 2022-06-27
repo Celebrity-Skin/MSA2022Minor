2003017e767309c7dcd66e9612ee0c613b72bedb
Celebrity-Skin <107645515+Celebrity-Skin@users.noreply.github.com>
2022-06-27 14:45:14 -0500

    Update student_minor.py

--- student_minor.py
+++ student_minor.py
@@ -1,76 +1,89 @@
-#print the menu
-print("Select option from Menu\n-----------------------")
-print("1. Login")
-print("2. Create User")
+# Print the menu
+from ast import Break
+from re import U
 
+
+print("Select option from Menu\n-----------------------")
+print("1. Login\n2. Create User")
 #prompt and get the option the user selected
 while True:
-    user_option = input("Would you like to (1) login or (2)create account? ")
-    #Ensure the user entered a valid option (1 or 2)
+    user_option = input("Would you like to (1) login or (2) Create Account?")
+    #ensure the user entered a valid option
     if user_option != "1" and user_option != "2":
-        # -if not, prompt user again
         print("\nERROR: Enter a 1 or 2")
         continue
     else:
-        print("YaY! Good input")
-        break     
+        print("OMG YOU DID IT :)")
+        break
+# if the user chose 1 ask for username and password
 
 
 if user_option == "1":
     while True:
-        #If user chose 1, ask for user name and password and
-        user_name = input("Please enter your user name: ")
-        user_pass = input("Please enter your password: ")
-        # - validate username and password combination in the users.txt file
-        #open the users files
+        user_name = input("Please enter your Username:")
+        user_pass = input("Please enter your Password:")
+        #validate username and password
+        #open username file
         user_file = open("users.txt", "r")
         user_found = False
-
-        #read the lines from the file
         for line in user_file:
             credentials = line.split(", ")
-             #compare username and password for a match
-            if user_name == credentials[0] and user_pass == credentials[1].rstrip():
+            if user_name == credentials[0] and user_pass == credentials[1]:
                 user_found = True
-                break
-
+                
         if user_found:
-            # - if valid then move on to prompt for student data
-            print(f"User {user_name} successfully logged in!\n")
+            print(f"User {user_name} is sucessfully logged in!")
             break
         else:
-            # - if not valid combination reprompt the user. 
-            print(f"User {user_name} not found!\n")
- 
-#If user chose 2, ask for user name and password and
+            print(f"User {user_name} not found")
+
+#   if not valid prompt the user
+#    If vcalid move on  to prompt for Studen information
+#if user chose 2 ask for username and password
 elif user_option == "2":
     while True:
-        #If user chose 2, ask for user name and password and
-        user_name = input("Please enter your user name (4 - 12 characters): ")
-        user_pass = input("Please enter your password: (6 - 16) characters: ")
-
-        #get username and password length
-        user_name_length = len(user_name)
+        user_name = input("Please enter your Username (4 to 12 characters):")
+        user_pass = input("Please enter your Password (6 to 16 characters):")
+        username_length = len(user_name)
         password_length = len(user_pass)
-        
-        # - validate username and password length. If valid, write to users.txt file
-        # 4 <= user_mame_length >= 12
-        if (user_name_length >= 4 and user_name_length <= 12) and (password_length >= 6 and password_length <= 16):
-            #write user and pass to the file
+    
+        #validate username and passworld langth, if valid write to user.txt file and move on
+        if username_length >=4 and username_length <=12 and password_length >=6 and password_length <=16:
             user_file = open("users.txt", "a")
             user_file.write(f"{user_name}, {user_pass}\n")
             user_file.close()
             break
         else:
-            print("ERROR: Incorrect username or password length.\n")
+            print("ERROR: Incorrect password or username length.\n")
 
 print("Ask user for student data")
-#Create 3 empty list for student name, scores, letter grades
-#Ask user how many students to enter data for
-#prompt user to enter student name and number score
-#store data somewhere
+# Make 3 empty lists for name, score, and letter grade
+student_names = []
+student_scores = []
+student_letter_grade = []
 
-#convert the number score to a letter grade 
+#prompt for number of students
+number_of_students = int(input("Enter the number of students to enter grades for:"))
+for counter in range(number_of_students):
+    #Promt user to enter name and number score
+    student_name = input("Enter student name:")
+    student_score = float(input("Enter student score:"))
 
-#Print student data(name, score, grade)
-#Calculate and print class average
+    #Store data in list
+    student_names.append(student_name)
+    student_scores.append(student_score)
+
+    #Convert to letter grade
+    if student_score <=100 and student_score >=90:
+        student_letter_grade.append("A")
+    elif student_score <90 and student_score >=80:
+        student_letter_grade.append("B")
+    elif student_score <80 and student_score >=70:
+        student_letter_grade.append("C")
+    elif student_score <70 and student_score >=60:
+        student_letter_grade.append("D")
+    else:
+        student_letter_grade.append("F")
+    #Print student date (name, score, grade)
+for index in range (number_of_students):
+    print(f"Student name: {student_names[index]},{student_scores[index]}, {student_letter_grade[index]}")
